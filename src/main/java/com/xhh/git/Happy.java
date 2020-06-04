4dcccc76891de684c89441c30d4bd4b1f2632c9b
wei-xhh <484613733@qq.com>
2020-06-04 17:43:46 +0800

    confict

--- src/main/java/com/xhh/git/Happy.java
+++ src/main/java/com/xhh/git/Happy.java
@@ -2,10 +2,7 @@ package com.xhh.git;
 
 public class Happy {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("right...");
-=======
 		System.out.println("left...");
->>>>>>> branch 'master' of https://github.com/wei-xhh/test-git.git
 	}
 }
